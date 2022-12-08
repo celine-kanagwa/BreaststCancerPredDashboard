f8ebfb52703d4ba22f74caf2cc77ad4f15996107
celine-kanagwa <celinekanagwa@gmail.com>
2022-12-08 20:52:21 +0200

    msg

---
+++ Dashboard/MeetTeam.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image 
+
+
+
+def MeetTeam():
+
+    #<div style="text-align: center"> st.text("Meet the team") </div>
+    st.markdown("<h1 style='text-align: center; color:purple;'>Meet The Team</h1>", unsafe_allow_html=True)
+
+    
+
+
+    col1,col2 = st.columns(2)
+
+    with col1:
+         st.text("HIRWA Celine")
+
+         image = Image.open ('E:/PHOTOS/CELINE SANDRINE/679A9973_1.JPG')
+         st.image (image, caption='')
+
+
+     
+
+
+
+
+
+      
